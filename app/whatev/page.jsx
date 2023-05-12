1de5ba4708fe8ce60508f71cab3bf7f7b985b968
Jonathan B. Sabuncu <sundfo057@gmail.com>
2023-05-12 11:42:27 -0400

    fetching layouts, routing

---
+++ app/whatev/page.jsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'Um whatev',
+  description: 'This is custom metadata for this route'
+}
+
+export default function Page(){
+  return(
+    <>
+    <main className="p-8">
+    <h1 className="font-bold text-2xl" >Whatev</h1>
+    <p>Routes in Next.js are too easy.</p>
+    </main>
+    </>
+  )
+}
---
+++ components/shared/Footer.jsx
@@ -0,0 +1,6 @@
+export default function Footer () {
+
+  return (
+    <> <footer>&copy; {new Date().getFullYear()} Boca Code.</footer></>
+  )
+}
